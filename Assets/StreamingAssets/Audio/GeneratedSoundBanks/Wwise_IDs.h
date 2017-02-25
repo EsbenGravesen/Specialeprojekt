3d2f71cd47de4106708ccbb55c48ff5ad352b8e2
MANTRA\Esben <esben.gravesen@gmail.com>
2017-02-25 12:09:58 +0100

    Wwise-project 0.1.1

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,8 +11,97 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID ACCELERATION_PLAY = 3487251266U;
+        static const AkUniqueID ACCELERATION_STOP = 4088790428U;
+        static const AkUniqueID PUZZLES_PLAY = 4129441541U;
+        static const AkUniqueID PUZZLES_STOP = 1217550467U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace GAMESTATE
+        {
+            static const AkUniqueID GROUP = 4091656514U;
+
+            namespace STATE
+            {
+                static const AkUniqueID GAME = 702482391U;
+                static const AkUniqueID MENU = 2607556080U;
+            } // namespace STATE
+        } // namespace GAMESTATE
+
+        namespace PUZZLECOUNT
+        {
+            static const AkUniqueID GROUP = 1058781018U;
+
+            namespace STATE
+            {
+                static const AkUniqueID PUZZLE1 = 4237507622U;
+                static const AkUniqueID PUZZLE2 = 4237507621U;
+                static const AkUniqueID PUZZLE3 = 4237507620U;
+                static const AkUniqueID PUZZLE4 = 4237507619U;
+                static const AkUniqueID PUZZLE5 = 4237507618U;
+                static const AkUniqueID PUZZLE6 = 4237507617U;
+                static const AkUniqueID PUZZLE7 = 4237507616U;
+                static const AkUniqueID PUZZLE8 = 4237507631U;
+                static const AkUniqueID PUZZLE9 = 4237507630U;
+                static const AkUniqueID PUZZLE10 = 3251089378U;
+            } // namespace STATE
+        } // namespace PUZZLECOUNT
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace CYCLES
+        {
+            static const AkUniqueID GROUP = 3152155388U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CYCLE1 = 3152155326U;
+                static const AkUniqueID CYCLE2 = 3152155325U;
+                static const AkUniqueID CYCLE3 = 3152155324U;
+                static const AkUniqueID CYCLE4 = 3152155323U;
+                static const AkUniqueID CYCLE5 = 3152155322U;
+                static const AkUniqueID CYCLE6 = 3152155321U;
+                static const AkUniqueID CYCLE7 = 3152155320U;
+                static const AkUniqueID CYCLE8 = 3152155319U;
+            } // namespace SWITCH
+        } // namespace CYCLES
+
+        namespace ELEMENTS
+        {
+            static const AkUniqueID GROUP = 4196236882U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID SPHERE = 3478592334U;
+                static const AkUniqueID ZONE1 = 831766780U;
+                static const AkUniqueID ZONE2 = 831766783U;
+                static const AkUniqueID ZONE3 = 831766782U;
+            } // namespace SWITCH
+        } // namespace ELEMENTS
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID DISTZONESPHERE = 821026704U;
+        static const AkUniqueID PLAYERSPEED = 1493153371U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
     namespace BUSSES
     {
+        static const AkUniqueID _3D = 511093792U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
         static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
     } // namespace BUSSES
