e1faba0ad728a57443e7961f4e8b76f74f4dab1c
nicolaiwoodhouse91 <nicolai_woodhouse91@hotmail.com>
2017-02-25 12:45:51 +0100

    puzzlemanager er cool

--- Assets/Scripts/SphereManager.cs
+++ Assets/Scripts/SphereManager.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SphereManager : MonoBehaviour {
+    public Vector2 CycleTempo;
+    public static int ZoneAmount;
+    private float _tempo;
 
-	// Use this for initialization
+    [System.Serializable]
+    public struct point
+    {
+        public Color zoneColor;
+        public Vector2 position;
+        public int type;
+    }
+    public List<point> Zones = new List<point>();
+    public float tempo {
+        set {
+            _tempo = value;
+        }
+        get {
+            if (CycleTempo.y == 0)
+                return 1;
+            return _tempo * CycleTempo.x / CycleTempo.y;
+        }
+    }
+
 	void Start () {
-		
+        print(tempo);
+        transform.GetChild(0).GetComponent<OrbitManager>().initRot(tempo);
 	}
 	
 	// Update is called once per frame
@@ -20,5 +42,6 @@ public class SphereManager : MonoBehaviour {
         go.transform.position = transform.position;
         go.transform.rotation = Quaternion.identity;
         go.transform.SetParent(transform);
+        Zones.Add(new point());
     }
 }
