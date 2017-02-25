f5e3ce63d763b1db311c945626bea1f75f6cab9c
KristofferLH <kristoffer_lh@hotmail.com>
2017-02-25 12:41:23 +0100

    LINE DRAWER ( LineSpawnerTEst.cs)

---
+++ Assets/LAVLINJERDOTDK.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LAVLINJERDOTDK : MonoBehaviour {
+
+    public Vector3[] pos;
+    List<Vector3> list;
+	// Use this for initialization
+	void Start () {
+        list = new List<Vector3>();
+        for (int i = 0; i < pos.Length; i++)
+        {
+            list.Add(pos[i]);
+        }
+        GetComponent<LineSpawnerTEst>().LineDrawer(list);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
